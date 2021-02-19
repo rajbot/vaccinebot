d9bb69bf1bec21d9d43020f16d9458643c467cae
rajbot <raj@bitstream.io>
2021-02-18 21:52:47 -0800

    Update Sonoma parser

--- location_parsers/sonoma.py
+++ location_parsers/sonoma.py
@@ -22,71 +22,37 @@ def address_fixup(a):
     """Add missing zip codes. These are from geocoder lookups, but
     hardcoded here so you don't need a mapquest api key to run this module"""
 
-    a = a.replace("501 Asti Road, Cloverdale", "501 Asti Road, Cloverdale, CA 95425")
-    a = a.replace(
-        "9291 Old Redwood Highway #200, Windsor",
-        "9291 Old Redwood Highway #200, Windsor, CA 95492",
-    )
-    a = a.replace(
-        "5401 Snyder Lane, Rohnert Park", "5401 Snyder Lane, Rohnert Park, CA 94928"
-    )
-    a = a.replace(
-        "16405 River Road, Guerneville", "16405 River Road, Guerneville, CA 95446"
-    )
-    a = a.replace(
-        "1115 Vine Street, Healdsburg", "1115 Vine Street, Healdsburg, CA 95448"
-    )
-    a = a.replace(
-        "389 South McDowell Boulevard, Petaluma",
-        "389 South McDowell Boulevard, Petaluma, CA 94954",
-    )
-    a = a.replace(
-        "6340 Commerce Boulevard, Rohnert Park",
-        "6340 Commerce Boulevard, Rohnert Park, CA 94928",
-    )
-    a = a.replace(
-        "1350 Bennett Valley Road, Santa Rosa",
-        "1350 Bennett Valley Road, Santa Rosa, CA 95404",
-    )
-    a = a.replace(
-        "6633 Oakmont Drive, Santa Rosa", "6633 Oakmont Drive, Santa Rosa, CA 95409"
-    )
-    a = a.replace(
-        "100 Calistoga Road, Santa Rosa", "100 Calistoga Road, Santa Rosa, CA 95409"
-    )
-    a = a.replace(
-        "1799 Marlow Road, Santa Rosa", "1799 Marlow Road, Santa Rosa, CA 95401"
-    )
-    a = a.replace(
-        "2300 Mendocino Avenue, Santa Rosa",
-        "2300 Mendocino Avenue, Santa Rosa, CA 95403",
-    )
-    a = a.replace(
-        "2785 Yulupa Avenue, Santa Rosa", "2785 Yulupa Avenue, Santa Rosa, CA 95405"
-    )
-    a = a.replace(
-        "406 North Main Street, Sebastopol",
-        "406 North Main Street, Sebastopol, CA 95472",
-    )
-    a = a.replace(
-        "477 West Napa Street, Sonoma", "477 West Napa Street, Sonoma, CA 95476"
-    )
-    a = a.replace("9080 Brooks Road, Windsor", "9080 Brooks Road, Windsor, CA 95492")
-    a = a.replace(
-        "126 1st Street West, Sonoma", "126 1st Street West, Sonoma, CA 95476"
-    )
-    a = a.replace("20000 Broadway, Sonoma", "20000 Broadway, Sonoma, CA 95476")
-    a = a.replace(
-        "14630 Armstrong Woods Road, Guerneville",
-        "14630 Armstrong Woods Road, Guerneville, CA 95446",
-    )
+    d = {
+        "501 Asti Road, Cloverdale": "95425",
+        "9291 Old Redwood Highway #200, Windsor": "95492",
+        "5401 Snyder Lane, Rohnert Park": "94928",
+        "16405 River Road, Guerneville": "95446",
+        "1115 Vine Street, Healdsburg": "95448",
+        "389 South McDowell Boulevard, Petaluma": "94954",
+        "6340 Commerce Boulevard, Rohnert Park": "94928",
+        "1350 Bennett Valley Road, Santa Rosa": "95404",
+        "6633 Oakmont Drive, Santa Rosa": "95409",
+        "100 Calistoga Road, Santa Rosa": "95409",
+        "1799 Marlow Road, Santa Rosa": "95401",
+        "2300 Mendocino Avenue, Santa Rosa": "95403",
+        "2785 Yulupa Avenue, Santa Rosa": "95405",
+        "406 North Main Street, Sebastopol": "95472",
+        "477 West Napa Street, Sonoma": "95476",
+        "9080 Brooks Road, Windsor": "95492",
+        "126 1st Street West, Sonoma": "95476",
+        "20000 Broadway, Sonoma": "95476",
+        "14630 Armstrong Woods Road, Guerneville": "95446",
+        "1024 Prince Avenue, Healdsburg": "95448",
+    }
+
+    if a in d:
+        zip = d[a]
+        a = f"{a}, CA {zip}"
 
     # For Petaluma Health Center, the county lists the address for Petaluma Fitness Center
     # However, our records have the health center's address.
-    a = a.replace(
-        "680 Sonoma Mountain Parkway, Building 800, Petaluma",
-        "1179 N McDowell Blvd, Petaluma, CA 94954",
-    )
+    if a == "680 Sonoma Mountain Parkway, Building 800, Petaluma":
+        a = "1179 N McDowell Blvd, Petaluma, CA 94954"
 
     return a
 
