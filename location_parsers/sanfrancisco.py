ba2d0f4e6a549e2bdb9f843adc7c72b27868b545
Deon Hua <me@deonhua.com>
2021-02-23 18:24:46 -0800

    Update San Francisco scraper w/ new URL + layout (#7)
    * Update San Francisco scraper w/ new URL + layout
    
    * Fixup addresses using zipcode map

--- location_parsers/sanfrancisco.py
+++ location_parsers/sanfrancisco.py
@@ -12,9 +12,28 @@ from . import header_dict
 from . import County, Location
 
 
-county = County(
-    name="San Francisco", url="https://sf.gov/get-vaccinated-against-covid-19"
-)
+county = County(name="San Francisco", url="https://sf.gov/vaccine-sites")
+
+address_to_zip = {
+    "1001 Potrero Ave, Building 30, San Francisco": "94110",
+    "755 Font Blvd, San Francisco": "94132",
+    "1181 Golden Gate Ave, San Francisco": "94115",
+    "2401 Keith St, San Francisco": "94124",
+    "1490 Mason St, San Francisco": "94133",
+    "1351 24th Ave, San Francisco": "94122",
+    "55 Frida Kahlo Way, San Francisco": "94112",
+    "333 Turk St, San Francisco": "94102",
+    "7000 Coliseum Way, Oakland": "94621",
+    "747 Howard Street, San Francisco": "94103",
+    "901 Rankin Street, San Francisco": "94124",
+}
+
+
+def address_fixup(address):
+    if address in address_to_zip:
+        zip = address_to_zip[address]
+
+    return f"{address}, CA {zip}"
 
 
 # Returns a list of Location objects
@@ -25,11 +44,11 @@ def run():
     soup = BeautifulSoup(r.data, "lxml")
     locations = []
 
-    sites = soup.find_all("div", class_="sfgov-address")
+    sites = soup.find_all("div", class_="sfgov-service-card vaccine-site")
 
     for site in sites:
-        title = site.find(class_="sfgov-address__title")
-        name = title.string
+        title = site.find(class_="vaccine-site__title")
+        name = title.string.strip()
 
         title2 = site.find(class_="sfgov-address__location-name")
         if title2 is not None:
@@ -38,12 +57,12 @@ def run():
         if name == "":
             raise Exception("Could not parse location name")
 
-        address1 = site.find(class_="sfgov-address__line1")
-        address2 = site.find(class_="sfgov-address__city-state-zip")
-        address = ", ".join([address1.string, address2.string])
+        address = site.find(class_="vaccine-site__address").find("a").string
 
         if address == "":
             raise Exception("Could not parse location address")
+
+        address = address_fixup(address)
 
         locations.append(
             Location(name=name, address=address, county=county.name, url=None)
