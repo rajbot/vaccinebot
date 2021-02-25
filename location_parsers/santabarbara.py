93c90d83a71803985ea27e7969ef353a2a5ceb47
Deon Hua <me@deonhua.com>
2021-02-24 22:34:50 -0800

    Update Santa Barbara Scraper to reflect new formatting (#9)
    * Update Santa Barbara Scraper to reflect new formatting
    
    * comma

--- location_parsers/santabarbara.py
+++ location_parsers/santabarbara.py
@@ -24,7 +24,8 @@ def address_fixup(a):
         a += " 93454"
     elif a == "2320 South Broadway, Santa Maria, CA 93455":
         a = a.replace("93455", "93454")
-
+    elif a == "400 W Pueblo Street Santa Barbara, CA 93102":
+        a = "400 W Pueblo Street, Santa Barbara, CA 93102"
     return a
 
 
@@ -43,14 +44,23 @@ def run():
             continue
         if s.name == "div":
             break
-        # print(s)
 
-        strong = s.find("strong")
-        name = strong.string
+        # Some of the entries use <b> or <p> instead of <strong>
+        strong = s.find_all(["strong", "b"])
+        strong = strong[0]
+
+        # Using strong.string when the string includes <br/> will cause it to return None
+        name = strong.contents[0].strip()
 
         address = strong.parent.get_text()
+
+        # Some addresses are structured differently, so we need to go two levels up.
+        if address == name:
+            address = strong.parent.parent.get_text()
+
         address = address[len(name) :].strip()
         address = address.replace("at ", "")
+        address = address.replace("Lompoc CA", "Lompoc, CA")
         address = re.sub(r"(, CA( \d{5})?).*", r"\1", address)
         address = address_fixup(address)
 
