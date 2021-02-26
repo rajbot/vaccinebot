fcdc46cf856935590c8a35904a5a1b5c7ca0c081
Deon Hua <me@deonhua.com>
2021-02-25 20:33:25 -0800

    Add hours + URLs where they exist (#20)
    * Add hours + URLs where they exist
    
    * remove extra print

--- state_parsers/vaccinefinder.py
+++ state_parsers/vaccinefinder.py
@@ -13,6 +13,32 @@ from . import add_state, validate, trim_zip_code
 
 parser = Parser(name="Vaccine Finder", url=None)
 
+days = {
+    "mon": "Monday",
+    "tue": "Tuesday",
+    "wed": "Wednesday",
+    "thur": "Thursday",
+    "fri": "Friday",
+    "sat": "Saturday",
+    "sun": "Sunday",
+}
+
+# Hours comes in a format like:
+#   "hours_mon" : "09:00AM-08:00PM",
+#   "hours_tue" : ...,
+# We'll just format it as "Monday: 09:00AM-08:00PM\nTuesday ..."
+def format_hours(provider):
+    hours = ""
+
+    for short_date, date in days.items():
+        key = f"hours_{short_date}"
+
+        if provider.get(key) is not None:
+            hours += f"{date}: {provider.get(key)}\n"
+
+    return hours.strip()
+
+
 def run():
     path = os.path.expanduser("~/dev/feed-recordings/vaccine-finder/*.json")
 
@@ -23,7 +49,25 @@ def run():
         fh.close()
 
         providers = obj.get("providers")
-        for provider in providers:
+
+        for p in providers:
+            guid = p.get("guid")
+            if guid is None:
+                continue
+
+            provider_path = os.path.expanduser(
+                f"~/dev/feed-recordings/vaccine-finder/providers/{guid}.json"
+            )
+
+            # Attempt to find a file in providers/ that matches the GUID, as it has more data
+            # But if it doesn't, we'll just use what we have, since that has everything except hours/URLs
+            try:
+                fh = open(provider_path)
+                provider = json.load(fh)
+                fh.close()
+            except FileNotFoundError:
+                provider = p
+
             name = provider.get("name")
             if name is None:
                 continue
@@ -45,7 +89,7 @@ def run():
             if city is None:
                 continue
             city = city.strip()
-            
+
             zip_code = provider.get("zip")
             if zip_code is None:
                 continue
@@ -65,8 +109,12 @@ def run():
                 phone=provider.get("phone"),
                 lat=provider.get("lat"),
                 long=provider.get("long"),
+                # Fields from the higher fidelity data
+                url=provider.get("website"),
+                reservation_url=provider.get("prescreening_site"),
+                hours=format_hours(provider),
             )
-            
+
             if l not in locations:
                 locations.append(l)
 
