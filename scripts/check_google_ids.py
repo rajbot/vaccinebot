7930ae5ee198deb3f0a01beb07c08bec33dc0c0c
rajbot <raj@bitstream.io>
2021-03-13 21:47:21 -0800

    Add Stanford scraper

---
+++ scripts/check_google_ids.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import csv
+import json
+import os
+import sys
+import urllib3
+
+
+url = "https://api.vaccinateca.com/v1/locations.json"
+http = urllib3.PoolManager()
+r = http.request("GET", url)
+db = json.loads(r.data.decode("utf-8"))
+
+print(f'loaded {len(db["content"])} locations from vaccinateca')
+
+place_ids = {}
+for row in db["content"]:
+    vacca_id = row["id"]
+    place_id = row.get("google_places_id")
+    if place_id is not None:
+        place_ids[place_id] = vacca_id
+
+path = sys.argv[1]
+with open(path) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        place_id = row['Google Places ID']
+        vacca_id = place_ids.get(place_id)
+        if vacca_id is not None:
+            print("Match:", place_id, vacca_id, row)
---
+++ state_parsers/stanford.py
@@ -0,0 +1,74 @@
+# Parse vaccination site locations in CA ArcGIS feed
+
+# Run manually: python3 -m state_parsers.california
+
+import csv
+import os
+import json
+import re
+import time
+import urllib3
+from bs4 import BeautifulSoup
+
+from . import Parser, Location
+from . import header_dict, debug_print
+from . import add_state, validate
+
+parser = Parser(name="Stanford", url="https://stanfordhealthcare.org/discover/covid-19-resource-center/patient-care/safety-health-vaccine-planning.html")
+
+
+def address_fixup(a):
+    if re.search(r'\d{5}$', a):
+        return a
+
+    d = {
+        "2585 Samaritan Drive, Suite 303, San Jose": "95124",
+        "5565 W. Las Positas Blvd, Suite 150, Pleasanton": "94588",
+        "6121 Hollis St, Emeryville": "94608",
+        "341 Galvez Street, Stanford": "94305",
+        "505 Broadway, Redwood City": "94063",
+        "350 E Tasman Dr, San Jose": "95134",
+    }
+    zip = d[a]
+    a = a + ", CA " + zip
+
+    return a
+
+
+# Returns a list of json objects
+def run():
+    http = urllib3.PoolManager(headers=header_dict)  # set user-agent
+    r = http.request("GET", parser.url)
+
+    soup = BeautifulSoup(r.data, "lxml")
+    locations = []
+
+    p = soup.find('p', string="Vaccination Sites")
+    container = p.find_next('div', class_="container")
+    items = container.find_all('li')
+
+    for li in items:
+        parts = li.get_text().split('\n')
+        if ' at ' in parts[0]:
+            name, address = parts[0].split(' at ', 1)
+            hours = '\n'.join(parts[1:])
+        else:
+            name = 'Clinic at ' + parts[0].split(',')[0].strip().strip('*')
+            address = parts[0]
+            hours = '\n'.join(parts[1:])
+            if address.endswith('*'):
+                address = parts[1]
+                hours = '\n'.join(parts[2:-1])
+
+        address = address_fixup(address)
+        phone = "(650) 498-9000"
+
+        l = Location(name=name, address=address, hours=hours, phone=phone)
+        locations.append(l)
+
+    return locations
+
+
+if __name__ == "__main__":
+    locations = run()
+    debug_print(locations)
